3a50a7909843c1d5ae1f8202d5df54ca724c7ae5
Elyssa Sexton <easexton@CCU018194.coastal.edu>
2022-02-26 16:46:14 -0500

    Initial Comment

---
+++ flights/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import Airline, Flight
+
+# Register your models here.
+class AirlineAdmin(admin.ModelAdmin):
+    fields = ['airline_name', 'airline_code']
+
+class FlightAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Flight Information', {'fields': ['airline','flight_number']}),
+        ('Origin/Destination', {'fields': ['origin', 'destination']}),
+        ('Departure and Arrival Time', {'fields': ['departure', 'arrival'], 'classes':['collapse']})
+    ]
+
+
+admin.site.register(Airline, AirlineAdmin)
+admin.site.register(Flight, FlightAdmin)
